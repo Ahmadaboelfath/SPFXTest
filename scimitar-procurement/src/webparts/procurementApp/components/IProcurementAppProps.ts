7c668d4cfdbcafb5b3bf09707363039eddd69b02
ahmed aboel fath <ahmed.mahmoud6@vodafone.com>
2021-12-21 17:02:34 +0200

    project initialization

---
+++ scimitar-procurement/src/webparts/procurementApp/components/IProcurementAppProps.ts
@@ -0,0 +1,3 @@
+export interface IProcurementAppProps {
+  description: string;
+}
---
+++ scimitar-procurement/src/webparts/procurementApp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IProcurementAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ProcurementAppWebPartStrings' {
+  const strings: IProcurementAppWebPartStrings;
+  export = strings;
+}
